c6296b0c0a9d1480a81a1de1e6e60abbf46d4608
Gurashish Singh <gurashish1singh@gmail.com>
2022-12-12 11:55:28 -0500

    Added more graph traversal problems

--- data_structures/graph/traversals.py
+++ data_structures/graph/traversals.py
@@ -1,6 +1,10 @@
 from __future__ import annotations
 
-from collections import deque
+import sys
+from collections import (
+    defaultdict,
+    deque,
+)
 from typing import (
     Set,
     Union,
@@ -12,36 +16,37 @@ NODE = Union[str, int]
 
 def depth_first_traversal(graph: GRAPH, source_node: NODE) -> None:
     # FIFO
+    if source_node not in graph:
+        return
     stack = [source_node]
     while stack:
         current = stack.pop()
         print(current)
-        # Sanity check
-        if current in graph:
-            # Consider the neighbours of current now
-            neighbours = graph[current]
-            stack.extend(neighbours)
+        # Consider the neighbours of current
+        neighbours = graph[current]
+        stack.extend(neighbours)
 
 
 def breadth_first_traversal(graph: GRAPH, source_node: NODE) -> None:
     # FILO
+    if source_node not in graph:
+        return
     queue = deque()
     queue.append(source_node)
     while queue:
         current = queue.popleft()
         print(current)
-        if current in graph:
-            neighbours = graph[current]
-            queue.extend(neighbours)
+        neighbours = graph[current]
+        queue.extend(neighbours)
 
 
 def find_path_using_dfs(graph: GRAPH, source: NODE, destination: NODE) -> bool:
     # n -> number of nodes
-    # n^2 -> number of edges
+    # n^2 -> e -> number of edges
     # Time: O(n^2)
     # Space: O(n)
     # This code assumes that the graph is acyclic, as in we are not going to be trapped
-    # in an infinite loop
+    # in an infinite loop. DFS is a recursive approach.
     if source not in graph:
         return False
     elif source == destination:
@@ -77,7 +82,7 @@ def find_path_using_bfs(graph: GRAPH, source: NODE, destination: NODE) -> bool:
     return False
 
 
-def find_path_using_dfs_for_acyclic_graph(
+def find_path_using_dfs_for_cyclic_graph(
     graph: GRAPH, source: NODE, destination: NODE, seen: Set[NODE]
 ) -> bool:
     # We use a set (seen) to keep track of the visited nodes to avoid infinite loops
@@ -92,7 +97,7 @@ def find_path_using_dfs_for_acyclic_graph(
     seen.add(source)
     neighbours = graph[source]
     for neighbour in neighbours:
-        if find_path_using_dfs_for_acyclic_graph(graph, neighbour, destination, seen):
+        if find_path_using_dfs_for_cyclic_graph(graph, neighbour, destination, seen):
             return True
     return False
 
@@ -119,13 +124,9 @@ def find_path_using_bfs_for_acyclic_graph(graph: GRAPH, source: NODE, destinatio
 
 
 def convert_edge_list_to_adjacency_list(edge_list: list[list[NODE]]) -> GRAPH:
-    graph = {}
+    graph = defaultdict(list)
     for edge in edge_list:
         a, b = edge
-        if a not in graph:
-            graph[a] = []
-        if b not in graph:
-            graph[b] = []
         graph[a].append(b)
         graph[b].append(a)
     return graph
@@ -148,6 +149,8 @@ def explore_neighbours(graph: GRAPH, node: NODE, seen: Set[NODE]) -> bool:
         return False
 
     seen.add(node)
+    # DFS is a recursive approach and we call explore_neighbours for each neighbour of the
+    # node. Once all distinct nodes have been visited, the function exits returning True.
     for neighbour in graph[node]:
         explore_neighbours(graph, neighbour, seen)
     return True
@@ -186,6 +189,7 @@ def get_shortest_path_using_bfs(graph: GRAPH, source: NODE, destination: NODE) -
 
     seen = set(source)
     queue = deque()
+    # The queue will have the source and the weight/distance to the current node
     queue.append((source, 0))
     while queue:
         current_node, current_distance = queue.popleft()
@@ -196,6 +200,76 @@ def get_shortest_path_using_bfs(graph: GRAPH, source: NODE, destination: NODE) -
                 seen.add(neighbour)
                 queue.append((neighbour, current_distance + 1))
     return -1
+
+
+def count_islands(graph: GRAPH) -> int:
+    # Time complexity -> O(r*c), rows and columns
+    # Space complexity -> O(r*c)
+    islands = 0
+    seen = set()
+    for row in range(len(graph)):
+        for column in range(len(graph[0])):
+            if find_neighbouring_land(graph, row, column, seen):
+                islands += 1
+    return islands
+
+
+def find_neighbouring_land(graph: GRAPH, row: int, column: int, seen: Set[tuple[int, int]]) -> bool:
+    # This uses a DFS approach to find all neighbouring land
+    # We do boundary checks first
+    if row >= len(graph) or column >= len(graph[0]):
+        return False
+
+    # We check for water first and only then use the seen set for visited land
+    if graph[row][column] == "W":
+        return False
+
+    coordinates = (row, column)
+    if coordinates in seen:
+        return False
+    seen.add(coordinates)
+
+    find_neighbouring_land(graph, row + 1, column, seen)
+    find_neighbouring_land(graph, row - 1, column, seen)
+    find_neighbouring_land(graph, row, column + 1, seen)
+    find_neighbouring_land(graph, row, column - 1, seen)
+    return True
+
+
+def find_smallest_isalnd(graph: GRAPH) -> int:
+    # Time complexity -> O(r*c), rows and columns
+    # Space complexity -> O(r*c)
+    smallest_island = sys.maxsize
+    seen = set()
+    for row in range(len(graph)):
+        for column in range(len(graph[0])):
+            size = find_island_length(graph, row, column, seen)
+            # We need to check if size is greater than 0
+            if size:
+                smallest_island = min(smallest_island, size)
+    return smallest_island
+
+
+def find_island_length(graph: GRAPH, row: int, column: int, seen: Set[tuple[int, int]]) -> int:
+    # Boundary checks
+    if row >= len(graph) or column >= len(graph[0]):
+        return 0
+
+    # We check for water first and only then use the seen set for visited land
+    if graph[row][column] == "W":
+        return 0
+
+    coordinates = (row, column)
+    if coordinates in seen:
+        return 0
+    seen.add(coordinates)
+
+    size = 1
+    size += find_neighbouring_land(graph, row + 1, column, seen)
+    size += find_neighbouring_land(graph, row - 1, column, seen)
+    size += find_neighbouring_land(graph, row, column + 1, seen)
+    size += find_neighbouring_land(graph, row, column - 1, seen)
+    return size
 
 
 if __name__ == "__main__":
@@ -236,9 +310,10 @@ if __name__ == "__main__":
     # ]
     # graph = convert_edge_list_to_adjacency_list(edge_list)
     # print(graph)
-    # print(find_path_using_dfs_for_acyclic_graph(graph, "i", "m", set()))
+    # print(find_path_using_dfs_for_cyclic_graph(graph, "i", "m", set()))
     # print("*" * 80)
     # print(find_path_using_bfs_for_acyclic_graph(graph, "i", "m"))
+    # print("*" * 80)
 
     # Counting connected components
     # graph = {
@@ -255,11 +330,33 @@ if __name__ == "__main__":
     # print(largest_connected_components(graph))
 
     # Finding the shortest path
-    graph = {
-        "w": ["x", "v"],
-        "v": ["w", "z"],
-        "x": ["w", "y"],
-        "y": ["x", "z"],
-        "z": ["v", "y"],
-    }
-    print(get_shortest_path_using_bfs(graph, "w", "z"))
+    # graph = {
+    #     "w": ["x", "v"],
+    #     "v": ["w", "z"],
+    #     "x": ["w", "y"],
+    #     "y": ["x", "z"],
+    #     "z": ["v", "y"],
+    # }
+    # print(get_shortest_path_using_bfs(graph, "w", "z"))
+
+    # Count the distinct islands (connected components)
+    graph = [
+        ["W", "L", "W", "W", "L", "W"],
+        ["L", "L", "W", "W", "L", "W"],
+        ["W", "L", "W", "W", "W", "W"],
+        ["L", "W", "W", "L", "L", "W"],
+        ["W", "W", "W", "L", "L", "L"],
+        ["L", "W", "W", "W", "W", "W"],
+    ]
+    print(count_islands(graph))
+
+    # Return the length of the smallest island
+    graph = [
+        ["W", "L", "W", "W", "L", "W"],
+        ["L", "L", "W", "W", "L", "L"],
+        ["W", "L", "W", "W", "W", "W"],
+        ["L", "W", "W", "L", "L", "W"],
+        ["L", "W", "W", "L", "L", "L"],
+        ["W", "W", "W", "W", "W", "W"],
+    ]
+    print(find_smallest_isalnd(graph))
